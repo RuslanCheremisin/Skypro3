22337f2af90f6a359303e955fe977be4990a8fac
rus <ruslan.cheremisin@gmail.com>
2022-12-26 23:10:02 +0200

    ДЗ по библиотекам

--- src/main/java/com/example/skypro3/model/Ingredient.java
+++ src/main/java/com/example/skypro3/model/Ingredient.java
@@ -1,29 +1,13 @@
 package com.example.skypro3.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
 public class Ingredient {
     private final String ingredientName;
     private int ingredientQuantity;
     private final String measureUnit;
 
-    public Ingredient(String ingredientName, int ingredientQuantity, String measureUnit) {
-        this.ingredientName = ingredientName;
-        this.ingredientQuantity = ingredientQuantity;
-        this.measureUnit = measureUnit;
-    }
-
-    public String getIngredientName() {
-        return ingredientName;
-    }
-
-    public int getIngredientQuantity() {
-        return ingredientQuantity;
-    }
-
-    public String getMeasureUnit() {
-        return measureUnit;
-    }
-
-    public void setIngredientQuantity(int ingredientQuantity) {
-        this.ingredientQuantity = ingredientQuantity;
-    }
 }
